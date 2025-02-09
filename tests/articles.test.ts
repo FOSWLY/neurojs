54f550ecb6ba521134f7072c882b9fb0bf8a026a
Toil <62353659+ilyhalight@users.noreply.github.com>
2025-02-09 03:25:54 +0300

    test: added skip cookie based test if session_id cookie isn't set

--- tests/articles.test.ts
+++ tests/articles.test.ts
@@ -2,6 +2,8 @@ import { expect, test } from "bun:test";
 import { NeuroClient } from "../src";
 import type { ArticleSummarizeOpts } from "../src/types/yandex";
 
+const SESSION_ID_COOKIE = Bun.env.SESSION_ID_COOKIE;
+
 test("summarize articles (with hmac)", async () => {
   const client = new NeuroClient();
   const data: ArticleSummarizeOpts = {
@@ -24,26 +26,29 @@ test("summarize articles (with hmac)", async () => {
   expect(finishRes.title).not.toBe("");
 });
 
-test("summarize articles (with user cookie)", async () => {
-  const client = new NeuroClient({
-    sessionIdCookie: Bun.env.SESSION_ID_COOKIE,
-  });
-  const data: ArticleSummarizeOpts = {
-    url: "https://toil.cc",
-    extraOpts: {
-      bypassCache: true,
-    },
-  };
+test.skipIf(!SESSION_ID_COOKIE)(
+  "summarize articles (with user cookie)",
+  async () => {
+    const client = new NeuroClient({
+      sessionIdCookie: SESSION_ID_COOKIE,
+    });
+    const data: ArticleSummarizeOpts = {
+      url: "https://toil.cc",
+      extraOpts: {
+        bypassCache: true,
+      },
+    };
 
-  const res = await client.summarizeArticle(data);
+    const res = await client.summarizeArticle(data);
 
-  // console.log(res);
-  expect(res.statusCode).toEqual(1);
+    // console.log(res);
+    expect(res.statusCode).toEqual(1);
 
-  await Bun.sleep(res.pollIntervalMs);
+    await Bun.sleep(res.pollIntervalMs);
 
-  data.extraOpts!.sessionId = res.sessionId;
-  const finishRes = await client.summarizeArticle(data);
-  // console.log(finishRes);
-  expect(finishRes.title).not.toBe("");
-});
+    data.extraOpts!.sessionId = res.sessionId;
+    const finishRes = await client.summarizeArticle(data);
+    // console.log(finishRes);
+    expect(finishRes.title).not.toBe("");
+  },
+);
